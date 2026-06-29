866e0256a857f522d81b10ce4382fdf424cdd4c8
Rashid <156198021+rashidvaloringal@users.noreply.github.com>
2026-06-29 18:01:14 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// ================================================================
+// SUPABASE SECURITY CONFIGURATION
+// ================================================================
+
+window.ENV_SUPABASE_URL = "https://axjeqkeocbfemojomfjm.supabase.co"; // ← ഇവിടെ നിങ്ങളുടെ Supabase URL പേസ്റ്റ് ചെയ്യുക
+window.ENV_SUPABASE_KEY = "sb_publishable_JgdXpn2s46NfLu0wNHU_9w_Cj9ewUKE";             // ← ഇവിടെ നിങ്ങളുടെ Supabase Anon Key പേസ്റ്റ് ചെയ്യുക
