ebcb087cd53a040c1fba80a2306b71be11feb59c
Rashid <156198021+rashidvaloringal@users.noreply.github.com>
2026-07-01 08:21:42 +0530

    Update add tts config.js

--- config.js
+++ config.js
@@ -7,3 +7,5 @@
 
 window.ENV_SUPABASE_URL = "https://axjeqkeocbfemojomfjm.supabase.co";
 window.ENV_SUPABASE_KEY = "sb_publishable_JgdXpn2s46NfLu0wNHU_9w_Cj9ewUKE";
+// പബ്ലിക് എഡ്ജ് വോയിസ് ഗേറ്റ്‌വേ ലിങ്ക്
+window.ENV_EDGE_TTS_API = "https://edge-tts-proxy.vercel.app/api/tts";
